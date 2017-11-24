dbe4f4d90ed0b362a5e096140f2e80b9e838c9b9
Arhind <arhindg@gmail.com>
2017-11-24 23:57:30 +0530

    second day

---
+++ E1.py
@@ -0,0 +1,9 @@
+numArray = map(int, input().split()) # Get the input
+
+
+sum_integer = 0
+# write your logic to add these 4 numbers here
+for i in range(0,4):
+  sum_integer = sum_integer + numArray[i] 
+
+print(sum_integer) # Print the sum
---
+++ second.py
@@ -0,0 +1,14 @@
+# i=4
+# d=4.0
+# s="Arhind"
+
+num = int( input())
+sum = num + i
+print(sum) 
+ 
+num = float(input())
+sum = num + d
+print(sum)
+
+num = input()
+print(s+num)
