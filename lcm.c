e4578e58ce6a8b68023bfae530f75722d4270e4a
Arhind <arhindg@gmail.com>
2017-12-14 23:51:45 +0530

    LCM and HCF file

---
+++ lcm.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+ 
+float gcd(float, float);
+ 
+int main() {
+  float x, y, hcf, lcm;
+ 
+  printf("Enter two integers\n");
+  scanf("%f%f", &x, &y);
+ 
+  hcf = gcd(x, y);
+  lcm = (x*y)/hcf;
+ 
+  printf("Greatest common divisor of %f and %f= %f\n", x, y, hcf);
+  printf("Least common multiple of %f and %f = %f\n", x, y, lcm);
+}
+ 
+float gcd(float x, float y) {
+  if (x == 0) {
+    return y;
+  }
+ 
+  while (y != 0) {
+    if (x > y) {
+      x = x - y;
+    }
+    else {
+      y = y - x;
+    }
+  }
+ 
+  return x;
+}
