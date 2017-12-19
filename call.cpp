b12da6b7c55b25f85f4b425afbdbc66272ccd760
Arhind <arhindg@gmail.com>
2017-12-19 23:43:13 +0530

    call by reference

---
+++ call.cpp
@@ -0,0 +1,30 @@
+   /* calling a function to swap the values using variable reference.*/
+
+void swap(int &p, int &q) {
+   int temp;
+   temp = p;
+   p = q;   
+   q = temp;
+  
+   return;
+}
+
+#include <iostream>
+using namespace std;
+
+void swap(int &x, int &y);
+
+int main () {
+   int a = 100;
+   int b = 200;
+ 
+   cout << "Before swap, value of a :" << a << endl;
+   cout << "Before swap, value of b :" << b << endl;
+
+   swap(a, b);
+
+   cout << "After swap, value of a :" << a << endl;
+   cout << "After swap, value of b :" << b << endl;
+ 
+   return 0;
+}
